5835f9080b6412f053dc78d8977a55179d8464b2
00-00-00 <nexusa8@gmail.com>
2019-11-13 02:16:47 +0530

    Adding react navigation

---
+++ src/screens/about/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export class About extends React.Component {
+    render() {
+        return(
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text> About Page </Text>
+        </View>
+        );
+    }
+}
