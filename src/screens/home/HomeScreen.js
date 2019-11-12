6fa7d45d61ad08820f110e3a3b606f588472e6b2
00-00-00 <nexusa8@gmail.com>
2019-11-13 02:26:36 +0530

    use navigation prop to move to about page

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Text, 
+  Button } from 'react-native';
 
 export class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <Button 
+          title="About"
+          onPress={() => this.props.navigation.navigate('About')}/>
       </View>
     );
   }
